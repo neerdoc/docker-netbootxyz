b4679064608f0c5ea02f9cbb0af651c0d7a3f261
Gustav Johansson <gustav@neer.se>
2020-03-27 13:53:27 +0100

    Added check to prevent running on wrong architecture.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ "$(uname -m)" != "armv7l" ]]; then
+  echo "Only do this on a Raspberry Pi!"
+  exit 1
+fi
+
 image="nexus.neer.se/repository/neerdoc/netboot.xyz:3.11"
 
 docker build -f Dockerfile.armhf -t ${image} .
